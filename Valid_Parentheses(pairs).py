0a5f5f46d59aaf84d1467b4bb717fd53b809868e
Dheeraj Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2023-08-13 12:34:46 +0530

    Create Valid_Parentheses(pairs).py

---
+++ Valid_Parentheses(pairs).py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = [] # only use append and pop
+        pairs = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+        for bracket in s:
+            if bracket in pairs:
+                stack.append(bracket)
+            elif len(stack) == 0 or bracket != pairs[stack.pop()]:
+                return False
+
+        return len(stack) == 0   
