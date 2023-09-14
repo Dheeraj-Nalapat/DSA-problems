0cd59d03854fbe68353093eec115ebc2723e261f
Dheeraj Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2023-09-14 20:40:46 +0530

    Create Climbing_Stairs.py

---
+++ Climbing_Stairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one , two = 1 , 1
+
+        for i in range(n-1):
+            one = one + two
+            two = one - two
+        return one   
