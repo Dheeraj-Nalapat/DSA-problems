2582c9ed8ec7e34e8f83c5bd2d275b19379e621e
Dheeraj Nalapat <75929280+Dheeraj-Nalapat@users.noreply.github.com>
2023-09-16 12:23:05 +0530

    Create Maximum_Depth_of_Binary_Tree(Iterative).py

---
+++ Maximum_Depth_of_Binary_Tree(Iterative).py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        stack = [[root,1]]
+        res=1
+        while stack:
+            node,depth = stack.pop()
+            if node:
+                res = max(depth,res)
+                stack.append([node.left,depth+1])
+                stack.append([node.right,depth+1])
+        return res 
